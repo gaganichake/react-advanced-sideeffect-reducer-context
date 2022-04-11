f7fbc2431f04c8c0b625b7f78d5ba51c42a930f7
Gagan Ichake <gaganichake@yahoo.com>
2022-04-11 09:18:42 -0500

    Using useReducer & useEffect together

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,11 +14,21 @@ const emailReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+};
+
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -26,40 +36,48 @@ const Login = (props) => {
     isValid: false,
   });
 
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: false,
+  });
+
   // userEffect summary
-  useEffect(() => {
-    console.log("EFFECT RUNNING");
-
-    return () => {
-      console.log("EFFECT CLEANUP");
-    };
-  }, [passwordIsValid]); // Dependency veriables/props in array.
-
   // useEffect(() => {
-  //   // Checking Form validity after every 500 milliseconds only when email/password changes
-  //   const timer = setTimeout(() => {
-  //     console.log('setTimeout()');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);// Wait for 500 miliseconds after every key stroke
+  //   console.log("EFFECT RUNNING");
 
   //   return () => {
-  //     console.log('CLEANUP!');
-  //     clearTimeout(timer); // Reset the prevous timer before setTimeout() is executed again.
+  //     console.log("EFFECT CLEANUP");
   //   };
-  // }, [enteredEmail, enteredPassword]); // useEffect is executed only when these two dependencies change.
+  // }, [passwordIsValid]); // Dependency veriables/props in array.
+
+  const {isValid: emailIsValid} = emailState;
+  const {isValid: passwordIsValid} = passwordState;
+
+  useEffect(() => {
+    // Checking Form validity after every 500 milliseconds only when email/password changes
+    const timer = setTimeout(() => {
+      console.log('setTimeout()');
+      setFormIsValid(
+        emailIsValid && passwordIsValid
+      );
+    }, 500);// Wait for 500 miliseconds after every key stroke
+
+    return () => {
+      console.log('CLEANUP!');
+      clearTimeout(timer); // Reset the prevous timer before setTimeout() is executed again.
+    };
+  }, [emailIsValid, passwordIsValid]); // useEffect is executed only when these two dependencies change.
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(emailState.isValid && enteredPassword.trim().length > 6);
-  };
+    // setFormIsValid(emailState.isValid && passwordState.isValid);
+  }; 
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    // setFormIsValid(emailState.isValid && passwordState.isValid);
   };
 
   const validateEmailHandler = () => {
@@ -67,12 +85,12 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -94,14 +112,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
